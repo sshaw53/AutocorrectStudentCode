4839bb66fbc841aca6f43b328ce16fda1ba450f7
sierrashaw <sierra.shaw@menloschool.org>
2025-03-21 14:10:19 -0700

    struggling and forgetting to push after working

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * Autocorrect
@@ -20,6 +21,9 @@ public class Autocorrect {
      */
     String[] words;
     int threshold;
+    public static int nGram = 3;
+    public static int radix = 26;
+    public static int q = 131071;
     ArrayList<String> toReturn = new ArrayList<>();
     ArrayList<Integer> toReturnValues = new ArrayList<>();
 
@@ -34,6 +38,145 @@ public class Autocorrect {
      * @return An array of all dictionary words with an edit distance less than or equal
      * to threshold, sorted by edit distnace, then sorted alphabetically.
      */
+
+    public String[] runTest(String typed) {
+        int wordLen = words.length;
+        ArrayList[] sortedDict = new ArrayList[q];
+        for (int i = 0; i < q; i++) {
+            sortedDict[i] = new ArrayList();
+        }
+
+        // For every word in the dictionary, hash by n-grams
+        for (int i = 0; i < wordLen; i ++) {
+            // Get first hash and add it to the new data Structure
+            int hash = hash(words[i], nGram);
+            sortedDict[hash].add(words[i]);
+
+            // Continue to hash n-grams until none left
+            int toLook = nGram;
+
+            while (toLook < words[i].length()) {
+                // Remove the first letter of the current hash
+                hash = ((hash + q) - words[i].charAt(toLook - nGram) * (int)Math.pow(radix, nGram - 1) % q) % q;
+                // Add the next letter to the hash
+                hash = ((hash * radix) + words[i].charAt(toLook)) % q;
+                // Add to dictionary data structure
+                sortedDict[hash].add(words[i]);
+
+                toLook += 1;
+            }
+        }
+
+        // Create a hash set
+        HashSet<String> filtered = new HashSet<String>();
+
+        // Get first hash, add everything in that index to the hash set
+        int hash = hash(typed, nGram);
+        filtered.addAll(sortedDict[hash]);
+
+        // Continue through the rest of the word
+        int toLook = nGram;
+
+        while (toLook < typed.length()) {
+            // Remove the first letter of the current hash
+            hash = ((hash + q) - typed.charAt(toLook - nGram) * (int)Math.pow(radix, nGram - 1) % q) % q;
+            // Add the next letter to the hash
+            hash = ((hash * radix) + typed.charAt(toLook)) % q;
+            // Add to dictionary data structure
+            filtered.addAll(sortedDict[hash]);
+
+            toLook += 1;
+        }
+
+        // Convert to an ArrayList so that I can get values at each index
+        ArrayList<String> filteredArray = new ArrayList<>(filtered);
+
+        for (int i = 0; i < filteredArray.size(); i ++) {
+            // Call lev function on each filtered word and then sort
+            int[][] changes = new int[typed.length() + 1][filteredArray.get(i).length() + 1];
+            int lev = lev(changes, typed, filteredArray.get(i));
+            if (lev <= threshold) {
+                int insertIndex;
+
+                if (!toReturnValues.isEmpty()) {
+                    insertIndex = toReturnValues.size();
+
+                    if (lev < toReturnValues.get(toReturnValues.size() - 1)) {
+                        insertIndex = toReturnValues.size() - 1;
+
+                        while (insertIndex > 0 && lev < toReturnValues.get(insertIndex - 1)) {
+                            insertIndex -= 1;
+                        }
+                    }
+                    if (insertIndex > 0 && lev == toReturnValues.get(insertIndex - 1)) {
+                        while (insertIndex > 0 && lev == toReturnValues.get(insertIndex - 1) &&
+                                words[i].compareTo(toReturn.get(insertIndex - 1)) < 0) {
+                            insertIndex -= 1;
+                        }
+                    }
+
+                    else {
+                        insertIndex = toReturnValues.size();
+                    }
+
+                    toReturn.add(insertIndex, words[i]);
+                    toReturnValues.add(insertIndex, lev);
+                }
+                else {
+                    toReturn.add(words[i]);
+                    toReturnValues.add(lev);
+                }
+            }
+        }
+
+        System.out.println(toReturn.size());
+        for (String word: toReturn) {
+            System.out.println(word);
+        }
+
+        return toReturn.toArray(new String[0]);
+    }
+
+    public int hash(String str, int length) {
+        int hashed = 0;
+
+        for(int i = 0; i < length; i++) {
+            hashed = (radix * hashed + str.charAt(i)) % q;
+        }
+
+        return hashed;
+    }
+
+
+    public int lev(int[][] changes, String str1, String str2) {
+        int str1Len = str1.length();
+        int str2Len = str2.length();
+
+        changes[0][0] = 0;
+
+        for (int i = 1; i <= str1Len; i++) {
+            changes[i][0] = i;
+        }
+
+        for (int j = 1; j <= str2Len; j++) {
+            changes[0][j] = j;
+        }
+
+        for (int i = 1; i <= str1Len; i++) {
+            for (int j = 1; j <= str2Len; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    changes[i][j] = changes[i - 1][j - 1];
+                }
+                else {
+                    changes[i][j] = 1 + Math.min(changes[i - 1][j - 1], Math.min(changes[i][j - 1], changes[i - 1][j]));
+                }
+            }
+        }
+
+        return changes[str1Len][str2Len];
+    }
+
+    /*
     public String[] runTest(String typed) {
         int wordLen = words.length;
         for (int i = 0; i < wordLen; i ++) {
@@ -108,6 +251,7 @@ public class Autocorrect {
 
         return changes[str1Len][str2Len];
     }
+     */
 
     /**
      * Loads a dictionary of words from the provided textfiles in the dictionaries directory.
